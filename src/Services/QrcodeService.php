84ecf150015dcdfa71bca111ed77fc7afd8a1173
annicksome26 <annicksome26@gmail.com>
2022-03-04 10:58:35 +0000

    add a logo in my qr-code

--- src/Services/QrcodeService.php
+++ src/Services/QrcodeService.php
@@ -6,7 +6,8 @@ use Endroid\QrCode\Builder\BuilderInterface;
 use Endroid\QrCode\Color\Color;
 use Endroid\QrCode\Encoding\Encoding;
 use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
-
+use Endroid\QrCode\Label\Alignment\LabelAlignmentCenter;
+use Endroid\QrCode\Label\Margin\Margin;
 
 /**Classe Qrcode sservice */
 class QrcodeService
@@ -29,6 +30,8 @@ class QrcodeService
         $objDateTime = new \DateTime('NOW');
         $dateString = $objDateTime->format('d-m-Y H:i:s');
 
+        $path = \dirname(__DIR__, 2).'/public/assets/';
+
         // set Qrcode
         $result = $this->builder
             ->data($url.$query)
@@ -37,6 +40,11 @@ class QrcodeService
             ->size(400)
             ->margin(10)
             ->labelText($dateString)
+            ->labelAlignment( new LabelAlignmentCenter())
+            ->labelMargin(new Margin(15, 5,5,5))
+            ->logoPath($path.'img/logo1.png')
+            ->logoResizeToWidth('100')
+            ->logoResizeToHeight('100')
             ->backgroundColor(new Color(243, 189, 16))
             ->build()
         ;
@@ -45,7 +53,7 @@ class QrcodeService
         $namePng = uniqid('', ''). '.png';
 
         // Save the img png 
-        $result->saveToFile((\dirname(__DIR__, 2).'/public/assets/qr-code/'.$namePng));
+        $result->saveToFile($path.'qr-code/'.$namePng);
 
         // return the reponse
         return $result->getDataUri();
